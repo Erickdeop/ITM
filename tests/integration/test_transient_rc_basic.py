7835a71e81d4a63c617c31f2d4488542cf22b1e8
ka-lucas <katarinelucas1@gmail.com>
2025-11-19 20:43:11 -0300

    ajuste teste

--- tests/integration/test_transient_rc_basic.py
+++ tests/integration/test_transient_rc_basic.py
@@ -6,12 +6,15 @@ from simulator.elements.base import TimeMethod
 def test_transient_rc_step():
     data = parse_netlist("circuits/rc_sine_parallel.net")
 
+    # vetor inicial corrigido
+    v0 = np.zeros(data.max_node + 1)
+
     t, out = solve_tran(
         data,
         total_time=0.01,
         dt=1e-4,
         nr_tol=1e-6,
-        v0_vector=None,
+        v0_vector=v0,
         desired_nodes=[1],
         method=TimeMethod.BACKWARD_EULER
     )
@@ -20,5 +23,6 @@ def test_transient_rc_step():
 
     # deve subir
     assert v[10] > v[0]
+
     # deve aproximar-se de algum valor estável
     assert abs(v[-1] - v[-2]) < 1e-3

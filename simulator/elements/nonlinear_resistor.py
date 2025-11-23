b2c0ccc664444c160a60b34a0588dcac978d56df
LuizaPac <luizacpacheco@hotmail.com>
2025-11-23 20:26:08 -0300

    docs: error messages and comments

--- simulator/elements/nonlinear_resistor.py
+++ simulator/elements/nonlinear_resistor.py
@@ -16,31 +16,27 @@ class NonLinearResistor(Element):
 
     def _get_current_and_conductance(self, Vab: float) -> Tuple[float, float]:
         """
-        Calcula I e G_eq (Condutância Equivalente) para a tensão Vab usando PWL.
+        Calculate the equivalent current and conductance for the Non-Linear Resistor
         """
-        # 1. Encontra a qual segmento Vab pertence
+        # Get Vab and find the segment
         if Vab <= self.V_points[0]:
             idx = 0
         elif Vab >= self.V_points[-1]:
             idx = len(self.V_points) - 2
         else:
-            # Encontra o índice 'i' onde V_points[i] <= Vab < V_points[i+1]
             idx = np.searchsorted(self.V_points, Vab) - 1
 
-        # 2. Segmento (Vi, Ii) a (Vi+1, Ii+1)
+        # Get points of the segment
         V_i, I_i = self.V_points[idx], self.I_points[idx]
         V_i1, I_i1 = self.V_points[idx+1], self.I_points[idx+1]
         
-        # 3. Calcula a Condutância (G_eq) do segmento (a derivada I/V)
+        # Get the equivalent conductance (G_eq) as the slope of the segment
         if V_i1 == V_i:
-            G_eq = 0.0 # Caso extremo (linha vertical)
+            G_eq = 0.0
         else:
             G_eq = (I_i1 - I_i) / (V_i1 - V_i)
         
-        # 4. Calcula a Corrente Equivalente (I_eq) para o método NR (I_eq = I - G_eq * V)
-        # O modelo Norton equivalente é: I = G_eq * V + I_N
-        # I_N (source) = I - G_eq * V  
-        # Corrente do Elemento (I_resistor) = I_i + G_eq * (Vab - V_i)
+        # Calculate the equivalent current source (I_eq) for the Norton model
         I_resistor = I_i + G_eq * (Vab - V_i)
         
         # I_NR = I_resistor - G_eq * Vab
@@ -54,38 +50,32 @@ class NonLinearResistor(Element):
                  x_guess: np.ndarray
                  ) -> Tuple[np.ndarray, np.ndarray]:
         """
-        Estampa para a análise DC, dependendo da estimativa atual de x (voltages).
-        A estamparia é feita pelo modelo equivalente Norton para NR:
-        I_NR = G_eq * V_ab + I_N
-        Onde:
-        - G_eq é a condutância tangente (derivada I/V)
-        - I_N é a fonte de corrente equivalente (residual)
+        DC analisis stamping for Non-Linear Resistor.
+        Uses Norton equivalent model based on current voltage guess.
+        Where:
+        - G_eq is the equivalent conductance matrix
+        - I_N is the equivalent current source vector
+        - x_guess is necessary, guess vector from Newton-Raphson solver
         """
 
         if x_guess is None:
-            # Se for chamado sem chute, significa que o solver DC LINEAR foi usado (erro)
+            # If no guess is provided, we cannot linearize the resistor
             raise RuntimeError(
-                "NonLinearResistor requer o solver Newton-Raphson (x_guess != None)."
+                "\033[91mNonLinearResistor requer o solver Newton-Raphson (x_guess != None).\033[00m"
             )
         
         V_ab = x_guess[self.a] - x_guess[self.b]
         
-        # Calcula a corrente equivalente I_N e a condutância G_eq
         I_N, G_eq = self._get_current_and_conductance(V_ab)
 
-        # Estamparia (Método MNA com modelo Norton equivalente)
-        # 1. Estampar G_eq
         G[self.a, self.a] += G_eq; G[self.b, self.b] += G_eq
         G[self.a, self.b] -= G_eq; G[self.b, self.a] -= G_eq
 
-        # 2. Estampar a fonte de corrente I_N
-        I[self.a] -= I_N # Sinais: I sai do nó a
-        I[self.b] += I_N # I entra no nó b
+        I[self.a] -= I_N
+        I[self.b] += I_N
 
         return G, I
     
-    # Em um Resistor Não-Linear, a estamparia Transiente é idêntica à DC,
-    # pois não há componentes reativos (C ou L) que exijam integração temporal.
     def stamp_transient(self, G, I, state, t, dt, method, x_guess: np.ndarray):
         G, I = self.stamp_dc(G, I, x_guess)
         return G, I, state

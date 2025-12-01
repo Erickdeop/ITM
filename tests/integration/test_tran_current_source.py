ce0713b0d0c8baf24ccbb410044d52d7723422d0
LuizaPac <luizacpacheco@hotmail.com>
2025-12-01 00:03:33 -0300

    feat: tests fixed to use new read from .net feature

--- tests/integration/test_tran_current_source.py
+++ tests/integration/test_tran_current_source.py
@@ -4,7 +4,7 @@ from simulator.elements.base import TimeMethod
 
 def test_tran_current_source_resistor_peak_voltage():
     c = Circuit('circuits/r_current_source_ac.net')
-    t, out = c.run_tran([1], total_time=5e-3, dt=1e-5, method=TimeMethod.TRAPEZOIDAL)
+    t, out = c.run_tran([1])
     v = out[0]
     vmax = float(np.max(np.abs(v[int(0.5*len(v)):])))
     assert 0.9 <= vmax <= 1.1
